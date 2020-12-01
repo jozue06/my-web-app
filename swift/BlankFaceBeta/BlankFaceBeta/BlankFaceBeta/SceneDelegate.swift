46445d1cab6f31c8d9ef8354a1674de8b8eeba49
jozue06 <jozue06@gmail.com>
2020-11-30 21:24:13 -0800

    fixes

--- swift/BlankFaceBeta/BlankFaceBeta/BlankFaceBeta/SceneDelegate.swift
+++ swift/BlankFaceBeta/BlankFaceBeta/BlankFaceBeta/SceneDelegate.swift
@@ -41,7 +41,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        
+        chatHelper.setSockets()
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
